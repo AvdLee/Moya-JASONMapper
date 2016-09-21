58dfa72c79403d5f8359f3e1dc107db9a68946eb
Antoine van der Lee <a.vanderlee@triple-it.nl>
2016-09-21 15:14:25 +0200

    Removing pods from git

--- Example/Pods/Target Support Files/Pods-Moya-JASONMapper_Example/Pods-Moya-JASONMapper_Example-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Moya_JASONMapper_Example : NSObject
-@end
-@implementation PodsDummy_Pods_Moya_JASONMapper_Example
-@end
--- Example/Pods/Target Support Files/Pods-Moya-JASONMapper_Tests/Pods-Moya-JASONMapper_Tests-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_Moya_JASONMapper_TestsVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_Moya_JASONMapper_TestsVersionString[];
-
